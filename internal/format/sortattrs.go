de29bb23f49841d744af9ca2eee7f4b00b6add5a
rsookram <rashadsookram@gmail.com>
2019-10-26 14:31:43 -0400

    Update rules for sorting attributes

--- internal/format/sortattrs.go
+++ internal/format/sortattrs.go
@@ -5,40 +5,89 @@ import (
 	"sort"
 )
 
-var priorities map[string]int
-
-func init() {
-	priorities = attrPriorities()
+var nsPriority = []string{
+	"xmlns",
+	"http://schemas.android.com/apk/res/android",
+	"http://schemas.android.com/apk/res-auto",
+	"http://schemas.android.com/tools",
+	"", // for attributes without a namespace like style or layout
 }
 
-type byPriority []xml.Attr
-
-func (s byPriority) Len() int {
-	return len(s)
-}
-func (s byPriority) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-func (s byPriority) Less(i, j int) bool {
-	fst := s[i]
-	snd := s[j]
-
-	fstP, fstContains := priorities[fst.Name.Local]
-	sndP, sndContains := priorities[snd.Name.Local]
-
-	if fstContains && sndContains {
-		return fstP < sndP
-	}
-
-	// Fallback to lexicographical order
-	return fst.Value < snd.Value
+var androidPriority = []string{
+	"id",
+	"layout_width",
+	"layout_height",
 }
 
+// sortAttrs returns a sorted slice of the given attributes in the following
+// order:
+//
+//   - xmlns:android
+//   - xmlns:* (alphabetic)
+//   - android:id
+//   - android:layout_width
+//   - android:layout_height
+//   - android:* (alphabetic)
+//   - app:* (alphabetic)
+//   - tools:* (alphabetic)
+//   - :* (alphabetic)
 func sortAttrs(attrs []xml.Attr) []xml.Attr {
 	sorted := make([]xml.Attr, len(attrs))
 	copy(sorted, attrs)
 
-	sort.Sort(byPriority(sorted))
+	sort.Slice(sorted, func(i, j int) bool {
+		return less(sorted[i].Name, sorted[j].Name)
+	})
 
 	return sorted
+}
+func less(fst, snd xml.Name) bool {
+	if fst.Space != snd.Space {
+		fstP, hasFst := indexOf(nsPriority, fst.Space)
+		sndP, hasSnd := indexOf(nsPriority, snd.Space)
+
+		if hasFst && hasSnd {
+			return fstP < sndP
+		}
+		if hasFst {
+			return true
+		}
+		if hasSnd {
+			return false
+		}
+	}
+
+	ns := fst.Space
+	switch ns {
+	case "xmlns":
+		// "android" is always first
+		if fst.Local == "android" && snd.Local != "android" {
+			return true
+		}
+		return fst.Local < snd.Local
+	case "http://schemas.android.com/apk/res/android":
+		fstP, hasFst := indexOf(androidPriority, fst.Local)
+		sndP, hasSnd := indexOf(androidPriority, snd.Local)
+
+		if hasFst && hasSnd {
+			return fstP < sndP
+		}
+		if hasFst {
+			return true
+		}
+		if hasSnd {
+			return false
+		}
+	}
+	return fst.Local < snd.Local
+}
+
+func indexOf(haystack []string, needle string) (int, bool) {
+	for i, s := range haystack {
+		if s == needle {
+			return i, true
+		}
+	}
+
+	return -1, false
 }
